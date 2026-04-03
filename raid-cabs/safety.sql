24fbebbd5ee82c5164cf08820f1403ac6f9ba926
SVJSimplify <Reach.Vikhyath@gmail.com>
2026-04-03 13:51:16 +0530

    Delete raid-cabs directory

--- raid-cabs/safety.sql
+++
@@ -1,8 +0,0 @@
- const firebaseConfig = {
-    apiKey: "AIzaSyCChvByd6SDEzzhsFmai-QYBe0hZ0x2-QA",
-    authDomain: "raid-cabs-29869.firebaseapp.com",
-    projectId: "raid-cabs-29869",
-    storageBucket: "raid-cabs-29869.firebasestorage.app",
-    messagingSenderId: "725918365928",
-    appId: "1:725918365928:web:d7fd600ced1e874d01b15d"
-  };
